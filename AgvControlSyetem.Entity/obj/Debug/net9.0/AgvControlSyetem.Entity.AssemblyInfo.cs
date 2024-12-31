b6894dd8e0880193563fe0604b123d19511f1597
商 黄 <tomdanhell@outlook.com>
2024-12-31 09:32:57 +0800

    1

--- AgvControlSyetem.Entity/obj/Debug/net9.0/AgvControlSyetem.Entity.AssemblyInfo.cs
+++ AgvControlSyetem.Entity/obj/Debug/net9.0/AgvControlSyetem.Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgvControlSyetem.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baec6d61b3c793d51a63a7624a82ea0e1436fff3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aec4a580c07304cd402cddb173d472018cadd7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgvControlSyetem.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgvControlSyetem.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
